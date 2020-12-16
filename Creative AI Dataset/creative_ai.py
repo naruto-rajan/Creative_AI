694fc2e1daa35bd7c6b228f7b1a7f712f596381a
naruto-rajan <rjnkumarsingh628@gmail.com>
2020-12-16 19:44:20 +0530

    Creative_AI

---
+++ Creative AI Dataset/creative_ai.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random 
+import os
+import PIL.Image
+import cv2
+import random
+from PIL import Image # Python Image Library is a library that adds support for opening, manipulating, and saving many different
+                      # image file formats
